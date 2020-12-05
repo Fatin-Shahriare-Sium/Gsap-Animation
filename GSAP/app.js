73a168fc9888567b31bbae2021d81edf89111470
Fatin-Shahriare-Sium <sium1206@gmail.com>
2020-12-05 20:56:45 +0600

    Gsap

--- GSAP/app.js
+++ GSAP/app.js
@@ -1,1 +1,61 @@
+let tl=gsap.timeline()
+
+tl.from(".t1",{duration:1.5,opacity:0,display:"none",fontWeigth:"blod"})
+.from(".t2",{duration:1.5,opacity:0,delay:1,fontWeigth:"blod",display:"none"})
+
+
+
+
+
+
+
+
+
+
+.to(".opener",{
+    duration:2,
+y:-1000,
+ease:Power2.easeOut,
+delay:0
+
+
+})
+
+
+
+
+
+
+.to("#pic",{
+    duration:3,
+    // opacity:.0,
+    // scaleX:2,
+    motionPath:{
+        path:[{x:500,y:100},{x:500,y:300},{x:-1,y:30},{x:1000,y:-150}],
+        curviness:1,
+        autoRotate:true
+    }
+})
+.to(".box",{
+    duration:2,
+    x:0,y:250
+
+})
+.to(".box",{
+    duration:1,
+    x:1050
+
+})
+.to("#pic",{
+    duration:3,
+    y:.0000000000001
+})
+.to(".box",{
+    duration:1,
+    y:180
+})
+
+
+
+
 
